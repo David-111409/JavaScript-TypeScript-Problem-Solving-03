c7cf12f71973386eba9e2192649b82e1cf55afa2
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-08 12:22:33 +0300

    Update 30_problem.js

--- 30_problem.js
+++ 30_problem.js
@@ -24,3 +24,15 @@ obj[Symbol.iterator] = function* () {
 for (let el of obj) {
     console.log(el);
 }
+
+function isPrimse(n) {
+    if (n <= 1) return false;
+    else if (n <= 3) return true;
+    else if (n % 2 === 0 || n % 3 === 0) return false;
+    for (let i = 5; i * i <= n; i += 6) {
+        if (n % i === 0 || n % (i + 2) === 0) return false;
+    }
+    return true;
+}
+
+console.log(isPrimse(7));

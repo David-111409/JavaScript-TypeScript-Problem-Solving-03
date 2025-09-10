fff6edd0f641067011d5cd60a5ff9c9009d23fc2
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-10 15:18:53 +0300

    Create 37_problem.js

---
+++ 37_problem.js
@@ -0,0 +1,20 @@
+const a = [0];
+console.log(a.length); // 1
+a[3] = 3;
+console.log(a.length); // 4
+
+for (let item of a) {
+    console.log(item); /* 0 undefined undefined 3 */
+}
+a.map((item) => {
+    console.log(item); // 0 3
+});
+a.forEach((item) => {
+    console.log(item); // 0 3
+});
+console.log(Object.keys(a));// '0' '3'
+delete a[3];
+console.log(a.length); // 4
+a[2] = 2;  // [0 , 3 * empty items]
+a.length = 1;
+console.log(a[0], a[1], a[2]); // 0 undefined undefined

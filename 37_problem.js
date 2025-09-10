fd48906ac92963305079200e8e1bc44778e4b004
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-10 15:22:34 +0300

    Update 37_problem.js

--- 37_problem.js
+++ 37_problem.js
@@ -18,3 +18,14 @@ console.log(a.length); // 4
 a[2] = 2;  // [0 , 3 * empty items]
 a.length = 1;
 console.log(a[0], a[1], a[2]); // 0 undefined undefined
+
+var x = Math.floor(Math.random());
+
+if (x > 0.5) {
+    var x = 1;
+} else {
+    // let x = 2;
+    var x = 2;
+}
+
+console.log(x);  // if let then x = 0 and if var x = 2

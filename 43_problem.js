4a07137fce214c548298e1ee72c4eeb14fcde452
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-11 14:42:08 +0300

    Update 43_problem.js

--- 43_problem.js
+++ 43_problem.js
@@ -1,3 +1,5 @@
 console.log(Array.from({ length: 10 }, (_, i) => i));
 console.log(Array(10).fill(0).map((_, i) => i));
 console.log([...Array(10).keys()]);
+console.log(Array.from([1, 2, 3], x => x * 2));
+console.log([1, 2, 3].map(x => x * 2));

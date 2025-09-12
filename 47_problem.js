2d28869612573f83b2ef8cfb872612ecf891f926
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-12 20:26:22 +0300

    Update 47_problem.js

--- 47_problem.js
+++ 47_problem.js
@@ -4,3 +4,9 @@ if (falsy /*This is true value*/) {
     if ([] /*This is true value*/) console.log("Array [] is empty");
     if ([] == false/*This is also true vlaue*/) console.log("here is the second condition!");
 }
+console.log(Boolean({}), Number({}), String({}));
+console.log(Boolean(+0));
+console.log(Boolean(-0));
+console.log(Boolean(NaN));
+console.log(Number(null));
+console.log(([] == false));

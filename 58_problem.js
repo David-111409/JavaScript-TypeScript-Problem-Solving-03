6a609a4b59ae2a4a6fcdf35cd81cd0a8d1705102
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-14 14:57:09 +0300

    Create 58_problem.js

---
+++ 58_problem.js
@@ -0,0 +1,72 @@
+let x = 31e-2;
+console.log(x);
+/**
+ validateSubsets([[1, 2], [2, 3], [1, 3]], [1, 2, 3]) ➞ true
+
+validateSubsets([[1, 2, 3], [2], [3], []], [1, 2, 3]) ➞ true
+
+validateSubsets([[1, 2], [2, 3], [1, 4]], [1, 2, 3]) ➞ false
+
+validateSubsets([[1, 2, 3, 4]], [1, 2, 3]) ➞ false
+ */
+
+// The two arrays are sorted.
+function validateSubsets(set, subset) {
+    let flat = [...new Set(set.flat())];
+    return String(flat) === String(subset);
+}
+console.log(
+    validateSubsets(
+        [
+            [1, 2],
+            [2, 3],
+            [1, 3],
+        ],
+        [1, 2, 3]
+    ),
+    true
+);
+console.log(validateSubsets([[1, 2, 3], [2], [3], []], [1, 2, 3]), true);
+console.log(
+    validateSubsets(
+        [
+            [1, 2],
+            [2, 3],
+            [1, 4],
+        ],
+        [1, 2, 3]
+    ),
+    false
+);
+console.log(validateSubsets([[1, 2, 3, 4]], [1, 2, 3]), false);
+console.log(
+    validateSubsets(
+        [
+            ["a", "b"],
+            ["b", "c"],
+            ["a", "c"],
+        ],
+        ["a", "b", "c"]
+    ),
+    true
+);
+console.log(
+    validateSubsets([["a", "b", "c"], ["b"], ["c"], []], ["a", "b", "c"]),
+    true
+);
+console.log(
+    validateSubsets(
+        [
+            ["a", "b"],
+            ["b", "c"],
+            ["a", "d"],
+        ],
+        ["a", "b", "c"]
+    ),
+    false
+);
+console.log(validateSubsets([["a", "b", "c", "d"]], ["a", "b", "c"]), false);
+console.log(validateSubsets([[true, false], [true]], [true, false]), true);
+console.log(validateSubsets([[true], [false], []], [true, false]), true);
+console.log(validateSubsets([[true], [true, false]], [true]), false);
+console.log(validateSubsets([[false]], [true]), false);

38a376c83e9f5e7dbfc09e44473c65264f19b0f8
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-15 15:44:06 +0300

    Update 63_problem.js

--- 63_problem.js
+++ 63_problem.js
@@ -13,3 +13,19 @@ function insertionSort(ar) {
 let ar = [-1, 2, 10, -2, 3, 5, -10];
 insertionSort(ar);
 console.log(ar);
+
+function quickSort(ar) {
+    let n = ar.length;
+    if (n <= 1) return ar;
+    let left = [];
+    let right = [];
+    let pivot = ar[n - 1];
+    for (let i = 0; i < n - 1; i++) {
+        if (ar[i] < pivot) left.push(ar[i]);
+        else right.push(ar[i]);
+    }
+    return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+let a = [-100, 2, 2, -2, 3, 10];
+console.log(quickSort(a));

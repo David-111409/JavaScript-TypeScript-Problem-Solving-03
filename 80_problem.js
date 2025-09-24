c5fc6b8afaf291bc17c8756f44fb2e8e0ddb0a56
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-24 11:52:29 +0300

    Update 80_problem.js

--- 80_problem.js
+++ 80_problem.js
@@ -34,3 +34,38 @@ var searchRange = function (nums, target) {
     return [first(nums, target), last(nums, target)];
 };
 console.log(searchRange([1, 1, 1, 1, 2, 3, 3, 3, 3, 4, 4], 4));
+
+
+var plusOne = function (digits) {
+    let l = digits.length,
+        i = 1;
+    let res = digits[l - i] + 1;
+    if (res < 10) {
+        digits[l - i] = res;
+        // return digits;
+    }
+    while (res === 10) {
+        digits[l - i] = 0;
+        i++;
+        res = digits[l - i] + 1 || 1;
+        if (l - i === -1) {
+            // digits.unshift(1);
+            // digits = [1, ...digits];
+            for (let k = l; k > 0 ; k--){
+                digits[k] = digits[k-1];
+            }
+            digits[0] = 1;
+            
+        } else digits[l - i] = res;
+    }
+    return digits;
+};
+
+console.log(plusOne([1, 2, 3]));
+console.log(plusOne([8]));
+console.log(plusOne([1, 0, 9]));
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+console.log(plusOne([9]));
+console.log(plusOne([9, 9]));
+console.log(plusOne([9, 9, 9]));
+

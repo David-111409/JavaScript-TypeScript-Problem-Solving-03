b901864ba9196000b21ee20a58b18bdb69d4cb7a
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-28 02:54:31 +0300

    Create 85_problem.js

---
+++ 85_problem.js
@@ -0,0 +1,39 @@
+log(0xe);
+log(0o14);
+// function sum(a, b) {
+//     return a + b;
+// }
+const sum = (a, b) => a + b;
+// function sub(a, b) {
+//     return a - b;
+// }
+const sub = (a, b) => a - b;
+
+function returnBack(fun) {
+    return `[${fun}]`;
+}
+
+// function mul(a, b) {
+//     return a * b;
+// }
+
+const mul = (a, b) => a * b;
+log(sum(1, 2), sub(1, 2));
+
+log(returnBack(sum(1, 2)));
+log(returnBack(sub(1, 2)));
+
+function calc(first, second, op) {
+    return `result = [${op(first, second)}]`;
+}
+
+log(calc(1, 2, sum));
+log(calc(1, 2, sub));
+log(calc(5, 2, mul));
+log(
+    calc(10, 2, function (first, second) {
+        return first / second;
+    })
+);
+
+log(calc(20, 5, (a, b) => a / b));

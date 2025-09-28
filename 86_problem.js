92094e985c6f829dc1cd2d5689cfbb66c838cdaa
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-09-28 18:44:32 +0300

    Create 86_problem.js

---
+++ 86_problem.js
@@ -0,0 +1,17 @@
+function isPrime(n) {
+    if (n <= 1) return false;
+    else if (n <= 3) return true;
+    else if (n % 2 === 0 || n % 3 === 0) return false;
+    for (let i = 5; i * i <= n; i += 6) {
+        if (n % i === 0 || n % (i + 2) === 0) return false;
+    }
+    return true;
+}
+
+var countPrimes = function (n) {
+    let count = 0;
+    for (let j = 2; j < n; j++) {
+        if (isPrime(j)) count++;
+    }
+    return count;
+};

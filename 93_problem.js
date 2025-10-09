c3c490525801a70cab5023a4dda9018925f40a32
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-09 23:47:04 +0300

    Update 93_problem.js

--- 93_problem.js
+++ 93_problem.js
@@ -13,7 +13,39 @@ console.log(findComplement(5));
 
 var bitwiseComplement = function(n) {
     let mask = (1 << n.toString(2).length) - 1;
+  
     return mask ^ n;
 };
 
 console.log( (0 ?? 1) || 2 ); // output: 2
+
+var removeElement = function (nums, val) {
+  let pos = 0;
+  let k = pos;
+  let l = nums.length;
+
+  for (let i = 0; i < l; i++) {
+    if (nums[i] !== val) {
+      nums[pos] = nums[i];
+      pos++;
+    }
+  }
+  
+  k = pos;
+
+  while (pos < l) {
+    nums[pos] = "_";
+    pos++;
+  }
+
+  return k;
+};
+
+let nums = [0, 1, 2, 2, 3, 0, 4, 2],
+  val = 2;
+
+// Output: 5, (nums = [0, 1, 4, 0, 3, _, _, _]);
+
+console.log(removeElement(nums, val));
+
+console.log(nums);

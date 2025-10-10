0975cda41188f714a0842e620f5d93325c1197d6
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-10 23:50:50 +0300

    Update 97_problem.js

--- 97_problem.js
+++ 97_problem.js
@@ -12,3 +12,44 @@ let check = locationsKeys.every(function (e) {
 }, mainLocation);
 
 console.log(check);
+
+// console.log(..."Dawoud", "Dawoud");
+// console.log([..."Dawoud"]);
+
+// let ar1 = [1, 2, 3];
+
+// let ar2 = [4, 5, 6];
+
+// let all = [...ar1, ...ar2];
+
+// let ar = [...ar1];
+
+// console.log(all, ar);
+
+// let allFriends = ["Osama", "Ahmed", "Sayed"];
+
+// let thisYearFriends = ["Sameh", "Mahmoud"];
+
+// // allFriends.push("Sameh", "Mahmoud");
+
+// allFriends.push(...thisYearFriends);
+
+// console.log(allFriends);
+
+// let myNums = [10, 20, -100, 100, 1000, 500];
+
+// console.log(Math.max(...myNums));
+
+let objOne = {
+  a: 1,
+  b: 2,
+};
+
+let objTwo = {
+  c: 3,
+  d: 4,
+};
+
+let obj = { ...objOne, ...objTwo, e: 5 };
+
+console.log(obj);

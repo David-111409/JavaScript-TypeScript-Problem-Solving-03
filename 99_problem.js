69a88d2bd4ff51ae01e812b7d80ac9b3768120d3
Muhammad Dawoud <muhammaddawoud610@gmail.com>
2025-10-12 03:42:39 +0300

    Update 99_problem.js

--- 99_problem.js
+++ 99_problem.js
@@ -26,3 +26,35 @@ console.log(secret("p*1") === `<p></p>`);
 console.log(secret("li*3") === `<li></li><li></li><li></li>`);
 console.log(secret("h1*4") === `<h1></h1><h1></h1><h1></h1><h1></h1>`);
 console.log(secret("ul*2"), `<ul></ul><ul></ul>`);
+
+// Example : 2
+/*
+RegEx: Boundary Assertions II
+You are given an array with random words but 
+your program doesn't accept words that begin with the capital letter "C". 
+Remove the unaccepted words and return the new array.
+
+Examples
+
+accepted(["Ducks", "Bears",  "Cats"]) ➞ ["Ducks", "Bears"]
+
+accepted(["cars", "trucks", "planes"] ➞ ["cars", trucks", "planes"]
+
+ ➞ ["Worms", "Bugs", "Beans"]
+Notes
+Use a RegEx boundary assertion in your function.
+ */
+
+// const accepted = (ar) => ar.filter((word) => !word.startsWith("C"));
+const acceptedWords = (ar) => ar.filter((word) => !/^C.*/.test(word));
+console.log(acceptedWords(["Cans", "Worms", "Bugs", "Cold", "Beans"]), [
+  "Worms",
+  "Bugs",
+  "Beans",
+]);
+console.log(acceptedWords(["Ducks", "Bears", "Cats"]), ["Ducks", "Bears"]);
+console.log(acceptedWords(["cars", "trucks", "planes"]), [
+  "cars",
+  "trucks",
+  "planes",
+]);
